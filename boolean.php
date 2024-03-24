82414765bddfba601f3a809b9bb62e618f79ee9b
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-24 23:37:32 +0800

    operator boolean

---
+++ boolean.php
@@ -0,0 +1,19 @@
+<?php
+    $a = 10;
+    $b = 30;
+
+    $c = $a > $b ;
+    $d = $a <= $b;
+
+    var_dump($c); // var_dum berfungsi untuk menampilkan identitas dari variabel tersebut
+    var_dump($d); // var_dum berfungsi untuk menampilkan identitas dari variabel tersebut
+
+
+    $aku = true;
+    $kamu = false;
+
+    $kita = $aku && $kamu;
+
+    var_dump($kita)
+
+?>
