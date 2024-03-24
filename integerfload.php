810c3156ba8eea7c37441767b2c516e77dfafa66
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-24 19:40:55 +0800

    round,increment dan deckrement

---
+++ integerfload.php
@@ -0,0 +1,14 @@
+<?php 
+    $a = 10;
+    $b = 990;
+
+    $c = $a / $b;
+
+    echo round($c, 2); // membulatkan dan menambahkan 2 angka di belakang koma
+
+    $d = 10;
+    $d++; // increment menambahkan keatas
+
+    $e = 10;
+    $e--; // deckrement menambahkan kebawah
+?>
