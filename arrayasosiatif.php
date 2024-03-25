2636e1e7e1b8dde20c2075ec52703e15b10c1b2f
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-25 10:42:59 +0800

    array asosiatif

---
+++ arrayasosiatif.php
@@ -0,0 +1,13 @@
+<?php 
+    $datadiri = [
+        "nama" => "rafi",
+        "umur" => 19,
+        "slogan" => "Aku ganteng",
+    ];
+
+    echo "Nama : " . " " . $datadiri["nama"];
+    echo "Umur : " . " " . $datadiri["umur"];
+    echo "Slogan : " . " " . $datadiri["slogan"];
+
+    
+?>
