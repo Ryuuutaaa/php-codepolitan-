9e6473982740238697d80aa415ed191e8cb71af9
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-25 10:47:41 +0800

    fore each in array

--- arrayasosiatif.php
+++ arrayasosiatif.php
@@ -1,13 +1,25 @@
 <?php 
-    $datadiri = [
-        "nama" => "rafi",
-        "umur" => 19,
-        "slogan" => "Aku ganteng",
+    // $datadiri = [
+    //     "nama" => "rafi",
+    //     "umur" => 19,
+    //     "slogan" => "Aku ganteng",
+    // ];
+
+    // echo "Nama : " . " " . $datadiri["nama"];
+    // echo "Umur : " . " " . $datadiri["umur"];
+    // echo "Slogan : " . " " . $datadiri["slogan"];
+
+
+
+    $alamat = [
+        "mahiru" => "malang",
+        "shiina" => "denpasar",
+        "itsuki" => "bali",
     ];
 
-    echo "Nama : " . " " . $datadiri["nama"];
-    echo "Umur : " . " " . $datadiri["umur"];
-    echo "Slogan : " . " " . $datadiri["slogan"];
+    foreach($alamat as $x => $value){
+        echo "Alamat : " . $x . " di " . $value . "<br>"; 
+    }
 
     
 ?>
