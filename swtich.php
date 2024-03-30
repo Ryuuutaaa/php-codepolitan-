8509bd35de64ea4c038388b60d03ab20756fb6b6
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-31 00:56:44 +0800

    swtich php

---
+++ swtich.php
@@ -0,0 +1,19 @@
+<?php
+$nilai = "";
+
+
+switch ($nilai) {
+    case "A":
+        echo "hebatt";
+        break;
+    case "B":
+        echo "lumayan";
+        break;
+    case "C":
+        echo "cukup";
+        break;
+
+    default:
+        echo "tidak ada nilai";
+        break;
+}
