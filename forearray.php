976f58b8e232c8fe0417d016fe82f5e9a6871fd0
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-04 20:36:58 +0800

    array php

---
+++ forearray.php
@@ -0,0 +1,14 @@
+<?php
+
+$warna = ["merah", "biru", "kuning", "oranye", "violet", "ingu", "hijau"];
+
+$jumlah_warna = 0;
+
+for ($i = 0; $i < 15; $i++) {
+    if ($warna[$i] == "violet") {
+        $jumlah_warna++;
+    }
+}
+
+
+echo "jumlah warna pada array sebanyak $jumlah_warna";
