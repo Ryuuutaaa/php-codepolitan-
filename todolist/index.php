602ebea2650f602c9bddf030f8ef3e1bcc0d184d
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-07 01:16:44 +0800

    add todo list

--- todolist/index.php
+++ todolist/index.php
@@ -1,29 +1,45 @@
-<h1>Todo List</h1>
-<form action="">
-    <label for="">Apa Kegiatan mu hari ini</label>
-    <input type="text" name="todo">
-    <button type="submit">Simpan</button>
+<?php
+$todos = [];
+$file = file_get_contents("todo.txt");
 
-    <ul>
-        <li>
-            <input type="checkbox" name="todo">
-            <label for="">Todo 1</label>
-            <a href="#">hapus</a>
-        </li>
-        <li>
-            <input type="checkbox" name="todo">
-            <label for="">Todo 1</label>
-            <a href="#">hapus</a>
-        </li>
-        <li>
-            <input type="checkbox" name="todo">
-            <label for="">Todo 1</label>
-            <a href="#">hapus</a>
-        </li>
-        <li>
-            <input type="checkbox" name="todo">
-            <label for="">Todo 1</label>
-            <a href="#">hapus</a>
-        </li>
-    </ul>
+if ($file) {
+    $todos = unserialize($file);
+}
+
+if (isset($_POST["todo"])) {
+    $data = $_POST["todo"];
+    $todos[] = [
+        'todo' => $data,
+        'status' => 0
+    ];
+
+    file_put_contents("todo.txt", serialize($todos));
+}
+
+if (isset($_POST['delete'])) {
+    $index = $_POST['delete'];
+    unset($todos[$index]);
+    $todos = array_values($todos);
+    file_put_contents("todo.txt", serialize($todos));
+}
+?>
+
+<h1>Todo List</h1>
+<form method="post">
+    <label for="">Apa Kegiatan mu hari ini</label>
+    <input type="text" name="todo" required>
+    <button type="submit">Simpan</button>
 </form>
+
+<ul>
+    <?php foreach ($todos as $index => $item): ?>
+        <li>
+            <input type="checkbox" name="status" <?php echo $item['status'] ? 'checked' : ''; ?>>
+            <label><?php echo htmlspecialchars($item['todo']); ?></label>
+            <form method="post" style="display:inline;">
+                <input type="hidden" name="delete" value="<?php echo $index; ?>">
+                <button type="submit" style="border:none; background:none; color:red; cursor:pointer;">hapus</button>
+            </form>
+        </li>
+    <?php endforeach; ?>
+</ul>
