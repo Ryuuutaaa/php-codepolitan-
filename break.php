9ba3a4b6871131cf6b47129604b8a6820da413e1
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-06 16:29:29 +0800

    feat : break

---
+++ break.php
@@ -0,0 +1,11 @@
+<?php
+
+$kru = ["toni", "kresna", "inda", "farhan", "adi"];
+foreach ($kru as $key => $nama) {
+    echo "Nama : $nama  <br>";
+
+    if ($naam == "farhan") {
+        echo "Farhan ada di index ke-$key <br>";
+        break;
+    }
+}
