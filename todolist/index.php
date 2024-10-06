fe5171488c395faa27a904d07d6b7b787e7f9fe6
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-07 01:07:12 +0800

    feat : started todolist

---
+++ todolist/index.php
@@ -0,0 +1,29 @@
+<h1>Todo List</h1>
+<form action="">
+    <label for="">Apa Kegiatan mu hari ini</label>
+    <input type="text" name="todo">
+    <button type="submit">Simpan</button>
+
+    <ul>
+        <li>
+            <input type="checkbox" name="todo">
+            <label for="">Todo 1</label>
+            <a href="#">hapus</a>
+        </li>
+        <li>
+            <input type="checkbox" name="todo">
+            <label for="">Todo 1</label>
+            <a href="#">hapus</a>
+        </li>
+        <li>
+            <input type="checkbox" name="todo">
+            <label for="">Todo 1</label>
+            <a href="#">hapus</a>
+        </li>
+        <li>
+            <input type="checkbox" name="todo">
+            <label for="">Todo 1</label>
+            <a href="#">hapus</a>
+        </li>
+    </ul>
+</form>
